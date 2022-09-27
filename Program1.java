1534ca0d85ef804156bf0ce1bf911e5803aabd6d
alkas2271 <114143635+alkas2271@users.noreply.github.com>
2022-09-27 09:33:14 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,29 @@
+package Package1;
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Program1
+{  
+  /*public static void main(String[] args)
+  {  
+	List<String>userData=new ArrayList<>();
+	System.out.println("Enter your data(send 'XDONE' to exit)");
+	Scanner sc=new Scanner(System.in);
+	while(true)
+	{
+		String input=sc.nextLine();
+		if("XDONE".equalsIgnoreCase(input))
+		{
+			break;
+		}
+	    userData.add(input);
+	}
+	System.out.printf("User Input data:\n %s",String.join("\n",userData));
+  }
+  */
+	
+	
+}
+
+
